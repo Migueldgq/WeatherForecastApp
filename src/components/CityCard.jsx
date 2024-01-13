c3c8236e2fe99115ec5f80d393a3b606b76764ed
Miguel Garcia <migueldariogq.mdg@gmail.com>
2024-01-13 01:42:59 +0100

    final

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -29,7 +29,9 @@ export const CityCard = ({ data, icon }) => {
             className="max-w-[2rem] absolute bottom-[-5px] left-[115px] "
           /> */}
 
-          <p className="font-medium">H:{data.main.temp_max}°C | L:{data.main.temp_min}°C</p>
+          <p className="font-medium">
+            H:{data.main.temp_max}°C | L:{data.main.temp_min}°C
+          </p>
         </div>
       </div>
       <div className="DetailBox flex flex-col justify-evenly w-[340px] h-[400px] rounded-md shadow-xl bg-slate-600 text-white p-6 ">
@@ -37,23 +39,47 @@ export const CityCard = ({ data, icon }) => {
         <div className="h-[276px] grid grid-cols-2 grid-rows-2 gap-4">
           <div className="bg-slate-400 rounded-md flex flex-col p-5 relative">
             <span className="Label font-normal text-md">Feels like</span>
-            <span className="Label-value font-semibold text-md ">{Math.round(data.main.feels_like)}°C</span>
-            <img src="/public/icons/thermometer.svg" alt="thermometericon" className="absolute max-w-[50px] left-[85px] top-[10px]" />
+            <span className="Label-value font-semibold text-md ">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+            <img
+              src="/icons/thermometer.svg"
+              alt="thermometericon"
+              className="absolute max-w-[50px] left-[85px] top-[10px]"
+            />
           </div>
           <div className="bg-slate-400 rounded-md flex flex-col p-5 relative">
             <span className="Label font-normal text-md">Pressure</span>
-            <span className="Label-value font-semibold text-md ">{Math.round(data.main.pressure)} hPa</span>
-            <img src="/public/icons/barometer.svg" alt="barometericon" className="absolute max-w-[50px] left-[85px] top-[10px]" />
+            <span className="Label-value font-semibold text-md ">
+              {Math.round(data.main.pressure)} hPa
+            </span>
+            <img
+              src="/icons/barometer.svg"
+              alt="barometericon"
+              className="absolute max-w-[50px] left-[85px] top-[10px]"
+            />
           </div>
           <div className="bg-slate-400 rounded-md flex flex-col p-5 relative">
             <span className="Label font-normal text-md">Wind </span>
-            <span className="Label-value font-semibold text-md">{Math.round(data.wind.speed)} km/s</span>
-            <img src="/public/icons/wind.svg" alt="windicon" className="absolute max-w-[50px] left-[85px] top-[10px]" />
+            <span className="Label-value font-semibold text-md">
+              {Math.round(data.wind.speed)} km/s
+            </span>
+            <img
+              src="/icons/wind.svg"
+              alt="windicon"
+              className="absolute max-w-[50px] left-[85px] top-[10px]"
+            />
           </div>
           <div className="bg-slate-400 rounded-md flex flex-col p-5 relative">
             <span className="Label font-normal text-md">Humidity</span>
-            <span className="Label-value font-semibold text-md">{Math.round(data.main.humidity)}%</span>
-            <img src="/public/icons/humidity.svg" alt="humidityicon" className="absolute max-w-[55px] left-[85px] top-[6px]" />
+            <span className="Label-value font-semibold text-md">
+              {Math.round(data.main.humidity)}%
+            </span>
+            <img
+              src="/icons/humidity.svg"
+              alt="humidityicon"
+              className="absolute max-w-[55px] left-[85px] top-[6px]"
+            />
           </div>
         </div>
       </div>
